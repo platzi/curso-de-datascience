f6b3e062f9e2a1110c470e3a7ed626ed2adce3c9
William Wolf <wwolf@shopkeep.com>
2016-09-20 11:50:13 -0400

    add data science principiantes course materials

---
+++ data-science-principiantes/inferencia_twitter/stream_listener.py
@@ -0,0 +1,15 @@
+from tweepy import StreamListener
+
+
+class FiniteStreamListener(StreamListener):
+    
+    def __init__(self, number_of_tweets):
+        self.number_of_tweets = number_of_tweets
+        self.tweets = []
+        super().__init__()
+        
+    def on_status(self, status):
+        if len(self.tweets) < self.number_of_tweets:
+            self.tweets.append(status.text)
+        else:
+            return False
